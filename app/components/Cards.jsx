7468455da4d12d4a6952e8b1d1db7f3f575ba184
seriesatul <atulsingh04895@gmail.com>
2025-08-06 20:17:42 +0530

    course part generation part

--- app/components/Cards.jsx
+++ app/components/Cards.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Card from './Card'
+import AddNewCourseDialogBox from '../workspace/_component/AddNewCourseDialogBox'
 
 function Cards() {
   return (
     <div className='w-full'>
         <div className='max-w-screen-xl mx-auto py-20 flex gap-3'>
-            <Card width={"basis-1/3"} headingFirst={"Elevate your Learning"} headingSecond={"Create New Course"} paragraph={"The smartest way to build your next curriculum"} hover={"bg-amber-300"}/>
+            <AddNewCourseDialogBox>
+                 <Card width={"basis-1/3"} headingFirst={"Elevate your Learning"} headingSecond={"Create New Course"} paragraph={"The smartest way to build your next curriculum"} hover={"bg-amber-300"}/>
+            </AddNewCourseDialogBox>
+           
+
+
             <Card width={"basis-2/3"} headingFirst={"Be more confident on your Expertise"} headingSecond={"Prepare for your next Interview"} paragraph={"Walk into any interview with total confidence."}/>
         </div>
     </div>
